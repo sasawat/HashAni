cf8d4be7ccf38bcb4cb920af4f457d37246727c6
Izumi Konata <sasawat@outlook.com>
2015-12-27 21:36:49 -0500

    It works!

---
+++ HashAni/HashAni.cs
@@ -0,0 +1,121 @@
+﻿using SharpShell.Attributes;
+using SharpShell.SharpContextMenu;
+using System;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace HashAni
+{
+    [ComVisible(true)]
+    [COMServerAssociation(AssociationType.AllFiles)]
+    public class HashAni : SharpContextMenu
+    {
+        private static UInt32[] table;
+        private const UInt32 seed = 0xffffffffu;
+        private const UInt32 poly = 0xedb88320u;
+        private byte[] buf;
+
+        protected override bool CanShowMenu()
+        {
+            return true;
+        }
+
+        protected override ContextMenuStrip CreateMenu()
+        {
+            //initialize menu
+            var menu = new ContextMenuStrip();
+            //initialize item
+            var item = new ToolStripMenuItem
+            {
+                Text = "Check CRC32 with HashAni"
+            };
+            item.Click += (sender, args) => checkCRC32();
+            //add item to menu
+            menu.Items.Add(item);
+            //return
+            return menu;
+        }
+
+        private void makeTable()
+        {
+            //we already have table...
+            if (table != null) return;
+
+            //init table
+            table = new UInt32[256];
+
+            //loop through calculating entries
+            for (UInt32 i = 0; i < 256; ++i)
+            {
+                UInt32 entry = i;
+                for (int j = 0; j < 8; ++j)
+                {
+                    if ((entry & 1) == 1) entry = (entry >> 1) ^ poly;
+                    else entry = entry >> 1;
+                }
+                table[i] = entry;
+            }
+        }
+
+        private UInt32 CRC32(FileStream fstr)
+        {
+            if (table == null) makeTable();
+            if (this.buf == null) this.buf = new byte[134217728];
+            UInt32 hash = seed;
+            while (fstr.Position < fstr.Length)
+            {
+                int rdsz = (int)(134217728 > fstr.Length - fstr.Position ? fstr.Length - fstr.Position : 134217728);
+                fstr.Read(buf, 0, rdsz);
+                for (int i = 0; i < rdsz; ++i)
+                {
+                    hash = (hash >> 8) ^ table[buf[i] ^ hash & 0xff];
+                }
+            }
+            return ~hash;
+        }
+
+        private string ui32ToStr(UInt32 ui32)
+        {
+            string ret = "";
+
+            byte[] a = BitConverter.GetBytes(ui32);
+            if (BitConverter.IsLittleEndian) Array.Reverse(a);
+
+            foreach (byte x in a)
+            {
+                ret += x.ToString("x2").ToUpper();
+            }
+
+            return ret;
+        }
+
+        private void checkCRC32()
+        {
+            string message = "";
+            bool error = false;
+            foreach (var f in SelectedItemPaths)
+            {
+                FileStream fstr = File.OpenRead(f);
+                string hash = ui32ToStr(CRC32(fstr));
+                string hashexpr = "[" + hash + "]";
+                Regex matcher = new Regex(hash);
+                if (!matcher.IsMatch(Path.GetFileName(f).ToUpper()))
+                {
+                    error = true;
+                    message += "ERROR: " + hash + " " + Path.GetFileName(f) + "\n";
+                }
+                fstr.Close();
+            }
+            if (error)
+            {
+                MessageBox.Show(message);
+            }
+            else
+            {
+                MessageBox.Show("All Match!");
+            }
+        }
+    }
+}
---
+++ HashAni/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("HashAni Anime Hash Check Utility")]
+[assembly: AssemblyDescription("Anime Fansub CRC32 Hash Check Utility")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("ミシガン大学　コンピューター工学部")]
+[assembly: AssemblyProduct("HashAni")]
+[assembly: AssemblyCopyright("psasawat@umich.edu")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("bf95828d-35f4-47ff-847d-e07135b54646")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("3.9.3.9")]
+[assembly: AssemblyFileVersion("3.9.3.9")]
